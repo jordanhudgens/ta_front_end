f1dac0c046d24ab9079018f9fb897105d8afafa5
Jordan Hudgens <jordan@wow.com>
2017-06-01 14:39:05 -0700

    Added initial design components

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}, :skip => [:registrations]
+
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
+  devise_scope :user do
+    get "/sign_in",  :to => "devise/sessions#new"
+    get "/sign_up",  :to => "devise/registrations#new"
+  end
+
   root to: 'static#home'
 end
